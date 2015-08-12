01f4a964898b19671c55977a7dc561116a486cb2
qianlifeng <scott.qian@fcgyl.cn>
2015-08-12 19:48:25 +0800

    update

--- src/main.go
+++ src/main.go
@@ -3,20 +3,31 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"strings"
 
-	"golang.org/x/sys/windows/registry"
+	"github.com/elazarl/goproxy"
 )
 
-func main() {
-	k, err := registry.OpenKey(registry.USERS, `Software\Microsoft\Windows\CurrentVersion\Internet Settings`, registry.ALL_ACCESS)
-	if err != nil {
-		log.Fatal(err)
+func isBaidu() goproxy.ReqConditionFunc {
+	return func(req *http.Request, ctx *goproxy.ProxyCtx) bool {
+		log.Print("Host:" + req.URL.Host)
+		log.Print("path:" + req.URL.Path)
+		log.Print("schema:" + req.URL.Scheme)
+		return strings.Contains(req.URL.Host, "oms.com")
 	}
-	defer k.Close()
+}
 
-	s, _, err := k.GetStringValue("ProxyServer")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Windows system root is %q\n", s)
+func main() {
+	proxy := goproxy.NewProxyHttpServer()
+	proxy.Verbose = false
+	proxy.OnRequest(isBaidu()).DoFunc(func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+		log.Print("url:" + r.URL.String())
+		res, err := http.Get("http://127.0.0.1:8080" + r.URL.Path)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		return r, res
+	})
+	log.Fatal(http.ListenAndServe(":8081", proxy))
 }
